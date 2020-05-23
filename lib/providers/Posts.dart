a4c003d4d25b5f52384c082866457a933b51daa4
Krishn <krishn.kumar2018@vitstudent.ac.in>
2020-05-23 20:56:13 +0530

    personalised feed

--- lib/providers/Posts.dart
+++ lib/providers/Posts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:followMe/models/Post.dart';
+import 'package:followMe/models/User.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -29,6 +30,16 @@ class Posts with ChangeNotifier {
 
   List<Post> get posts {
     return [..._posts];
+  }
+
+  List<User> _following2 = [];
+  List<String> _followingIds2 = [];
+
+  List<String> get followingIds2 {
+    _followingIds2 = _following2.map((e) => e.id).toList();
+    print("here");
+    print(_followingIds2);
+    return [..._followingIds2];
   }
 
   final String authToken;
@@ -78,20 +89,39 @@ class Posts with ChangeNotifier {
 
       final extractedData = json.decode(res.body) as Map<String, dynamic>;
       final List<Post> loadedPosts = [];
-
-      if (extractedData != null) {
-        extractedData.forEach((postId, postData) {
-          loadedPosts.insert(
-            0,
-            Post(
-                id: postId,
-                user: postData["user"],
-                post: postData["post"],
-                postDate: DateTime.parse(postData["postDate"]),
-                userdp: postData["userpic"]),
-          );
-        });
+      if (filterByUser) {
+        if (extractedData != null) {
+          extractedData.forEach((postId, postData) {
+            loadedPosts.insert(
+              0,
+              Post(
+                  id: postId,
+                  user: postData["user"],
+                  post: postData["post"],
+                  postDate: DateTime.parse(postData["postDate"]),
+                  userdp: postData["userpic"]),
+            );
+          });
+        }
       }
+      await fetchFollowingList2();
+      followingIds2.forEach((element) {
+        if (extractedData != null) {
+          extractedData.forEach((postId, postData) {
+            if (element == postData["creatorId"]) {
+              loadedPosts.insert(
+                0,
+                Post(
+                    id: postId,
+                    user: postData["user"],
+                    post: postData["post"],
+                    postDate: DateTime.parse(postData["postDate"]),
+                    userdp: postData["userpic"]),
+              );
+            }
+          });
+        }
+      });
 
       _posts = loadedPosts;
       notifyListeners();
@@ -134,5 +164,31 @@ class Posts with ChangeNotifier {
     http.delete(url);
     _posts.removeWhere((post) => post.id == id);
     notifyListeners();
+  }
+
+  //following list
+  Future<void> fetchFollowingList2() async {
+    final url =
+        'https://followme-a0fcb.firebaseio.com/followList.json?auth=$authToken&orderBy="followerId"&equalTo="$userId"';
+    try {
+      final res = await http.get(url);
+      final extractedData = json.decode(res.body) as Map<String, dynamic>;
+      final List<User> loadedUsers = [];
+      if (extractedData != null) {
+        extractedData.forEach((key, value) {
+          loadedUsers.insert(
+              0,
+              User(
+                  id: value["followingId"],
+                  name: value["followingName"],
+                  dpUrl: value["followingDp"]));
+        });
+      }
+      // print(res.body);
+      _following2 = loadedUsers;
+    } catch (error) {
+      print(error);
+      throw error;
+    }
   }
 }
--- lib/widgets/MyPosts.dart
+++ lib/widgets/MyPosts.dart
@@ -43,120 +43,124 @@ class _MyPostsState extends State<MyPosts> {
 
     return _isLoading
         ? LoadingSpinner()
-        : ListView.builder(
-            padding: EdgeInsets.only(left: 25.0, right: 20.0, top: 45.0),
-            itemCount: _posts.length,
-            itemBuilder: (ctx, i) {
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Card(
-                  elevation: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
+        : _posts.isEmpty
+            ? Center(child: Text("No Posts yet !"))
+            : ListView.builder(
+                padding: EdgeInsets.only(left: 25.0, right: 20.0, top: 45.0),
+                itemCount: _posts.length,
+                itemBuilder: (ctx, i) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Card(
+                      elevation: 5,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            CircleAvatar(
-                              backgroundImage: _posts[i].userdp != null
-                                  ? NetworkImage(_posts[i].userdp)
-                                  : null,
-                              backgroundColor: Colors.grey,
-                              child: _posts[i].userdp == null
-                                  ? Text(_posts[i].user.substring(0, 1))
-                                  : null,
+                            Row(
+                              children: <Widget>[
+                                CircleAvatar(
+                                  backgroundImage: _posts[i].userdp != null
+                                      ? NetworkImage(_posts[i].userdp)
+                                      : null,
+                                  backgroundColor: Colors.grey,
+                                  child: _posts[i].userdp == null
+                                      ? Text(_posts[i].user.substring(0, 1))
+                                      : null,
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  current.name,
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
                             ),
                             SizedBox(
-                              width: 5,
+                              height: 8,
                             ),
-                            Text(
-                              current.name,
-                              style: TextStyle(fontSize: 20),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Text(_posts[i].post),
                             ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text(_posts[i].post),
-                        ),
-                        ButtonBar(
-                          children: <Widget>[
-                            FlatButton(
-                              child: const Text('EDIT'),
-                              onPressed: () {
-                                myController.text = _posts[i].post;
-                                print(_posts[i].id);
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text("Edit"),
-                                      content: TextField(
-                                        decoration: InputDecoration(
-                                            labelText: "Edit your post"),
-                                        maxLines: 3,
-                                        keyboardType: TextInputType.multiline,
-                                        controller: myController,
-                                      ),
-                                      actions: <Widget>[
-                                        FlatButton.icon(
-                                            onPressed: () {
-                                              myController.text = "";
-                                              Navigator.of(context).pop();
-                                            },
-                                            icon: Icon(Icons.cancel),
-                                            label: Text("Cancel")),
-                                        FlatButton.icon(
-                                          onPressed: () async {
-                                            Navigator.of(context).pop();
-                                            setState(() {
-                                              _isLoading = true;
-                                            });
-                                            await Provider.of<Posts>(context,
-                                                    listen: false)
-                                                .updatePosts(
-                                              _posts[i].id,
-                                              Post(
-                                                  post: myController.text,
-                                                  user: current.id,
-                                                  userdp: current.dpUrl),
-                                            );
-                                            setState(() {
-                                              _isLoading = false;
-                                            });
-                                            myController.text = "";
-                                          },
-                                          label: Text("Edit"),
-                                          icon: Icon(Icons.call_made),
-                                        )
-                                      ],
+                            ButtonBar(
+                              children: <Widget>[
+                                FlatButton(
+                                  child: const Text('EDIT'),
+                                  onPressed: () {
+                                    myController.text = _posts[i].post;
+                                    print(_posts[i].id);
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text("Edit"),
+                                          content: TextField(
+                                            decoration: InputDecoration(
+                                                labelText: "Edit your post"),
+                                            maxLines: 3,
+                                            keyboardType:
+                                                TextInputType.multiline,
+                                            controller: myController,
+                                          ),
+                                          actions: <Widget>[
+                                            FlatButton.icon(
+                                                onPressed: () {
+                                                  myController.text = "";
+                                                  Navigator.of(context).pop();
+                                                },
+                                                icon: Icon(Icons.cancel),
+                                                label: Text("Cancel")),
+                                            FlatButton.icon(
+                                              onPressed: () async {
+                                                Navigator.of(context).pop();
+                                                setState(() {
+                                                  _isLoading = true;
+                                                });
+                                                await Provider.of<Posts>(
+                                                        context,
+                                                        listen: false)
+                                                    .updatePosts(
+                                                  _posts[i].id,
+                                                  Post(
+                                                      post: myController.text,
+                                                      user: current.id,
+                                                      userdp: current.dpUrl),
+                                                );
+                                                setState(() {
+                                                  _isLoading = false;
+                                                });
+                                                myController.text = "";
+                                              },
+                                              label: Text("Edit"),
+                                              icon: Icon(Icons.call_made),
+                                            )
+                                          ],
+                                        );
+                                      },
                                     );
                                   },
-                                );
-                              },
-                            ),
-                            FlatButton(
-                              child: const Text(
-                                'DELETE',
-                                style: TextStyle(color: Colors.red),
-                              ),
-                              onPressed: () {
-                                Provider.of<Posts>(context, listen: false)
-                                    .deletePosts(_posts[i].id);
-                              },
+                                ),
+                                FlatButton(
+                                  child: const Text(
+                                    'DELETE',
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  onPressed: () {
+                                    Provider.of<Posts>(context, listen: false)
+                                        .deletePosts(_posts[i].id);
+                                  },
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                },
               );
-            },
-          );
   }
 }
